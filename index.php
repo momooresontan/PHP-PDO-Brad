5ba9b06fe3488baaacc6318576207ca34cc75a95
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-27 14:15:22 +0100

    pdo initialization

--- index.php
+++ index.php
@@ -3,4 +3,12 @@
     $user = "momo";
     $password = "123456";
     $database = "pdo_posts";
+
+    $dsn = "mysql:host=".$host.";dbname=".$database;
+
+    //Create PDO instance
+    $pdo = new PDO($dsn, $user, $password);
+
+    //PDO QUERY
+    $stmt = $pdo->query("SELECT * FROM post");
 ?>
