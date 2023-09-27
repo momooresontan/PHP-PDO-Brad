6ed3e37151410507e1be3a3316b5a2bf6fc177dd
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-27 14:46:11 +0100

    pdo fetch

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
     //Create PDO instance
     $pdo = new PDO($dsn, $user, $password);
 
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
     //PDO QUERY
     $stmt = $pdo->query("SELECT * FROM post");
+
+    // while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    //     echo $row['title']."<br>";
+    // }
+    while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+        echo $row->title."<br>";
+    }
 ?>
